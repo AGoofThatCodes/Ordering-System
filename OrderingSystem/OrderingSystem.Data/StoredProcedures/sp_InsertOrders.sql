c2e5d9be2b35492fdee04c8aa704b1bbae21f167
Kirsty Selesho <KSelesho@you-source.co.za>
2022-07-21 16:06:37 +0200

    order taking system

---
+++ OrderingSystem/OrderingSystem.Data/StoredProcedures/sp_InsertOrders.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[sp_InsertOrders]
+	@item nvarchar(50),
+	@price decimal = 0,
+	@senderemail nvarchar(50),
+	@sendername nvarchar(50),
+	@recipientname nvarchar(50),
+	@recipientemail nvarchar(50),
+	@voucher integer = 0
+AS
+DECLARE @id uniqueidentifier = NEWID();
+	INSERT INTO
+	[dbo].[Orders] ([Item], [Price], [SenderEmail], [SenderName], [RecipientName],
+	[RecipientEmail], [Voucher], [Id])
+	VALUES
+	(@item, @price, @senderemail, @sendername, @recipientname, @recipientemail, @voucher, @id)
+RETURN 0
